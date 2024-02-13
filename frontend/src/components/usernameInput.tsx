efc82b41a4d3ecad470435a0ab62bdc1f8c6e79e
ining310 <h94usu6@gmail.com>
2024-02-14 05:09:22 +0800

    get usernameInput

--- frontend/src/components/usernameInput.tsx
+++ frontend/src/components/usernameInput.tsx
@@ -1,11 +1,19 @@
 import TextField from '@mui/material/TextField'
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import { useRef } from 'react';
 
 export default function UsernameInput() {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const handleUsername = async () => {
+    if (!usernameRef.current) return;
+    const newUsername = usernameRef.current.value;
+    console.log(newUsername);
+  };
+
   return (
     <div className='flex items-center justify-center'>
-        <TextField id="standard-basic" label="Username" variant="standard" />
-        <ArrowCircleRightOutlinedIcon className='mt-3 ml-2' />
+        <TextField id="standard-basic" label="Username" variant="standard" inputRef={usernameRef} />
+        <div className='mt-3 ml-2 cursor-pointer hover:bg-slate-100 rounded-full' onClick={handleUsername}><ArrowCircleRightOutlinedIcon /></div>
     </div>
   );
 }
