7246eacccca8a81b150d77962b3316d1940f877d
Shu-Chenliu <susanliu.singing@gmail.com>
2024-02-15 23:09:49 +0800

    p13 top-part

--- src/app/CourseHistory/actions.ts
+++ src/app/CourseHistory/actions.ts
@@ -2,7 +2,16 @@ export const getCourse = async(username:string) =>{
   "use server";
   const course= [{
     type:1,
+    topic:"主題一",
     name:"class 1",
+  },{
+    type:1,
+    topic:"主題2",
+    name:"class 2",
+  },{
+    type:1,
+    topic:"主題3",
+    name:"class 3",
   }];
   return course;
 }
--- src/app/CourseHistory/layout.tsx
+++ src/app/CourseHistory/layout.tsx
@@ -1,13 +1,16 @@
+import Header from "@/components/Header";
 type Props = {
   children: React.ReactNode;
 };
 
 function Layout({ children }: Props) {
   return (
-    // overflow-hidden for parent to hide scrollbar
-    <div className="w-full">
-      {children}
-    </div>
+    <>
+      <Header username={"Susan"}/>
+      <div className="w-full h-full">
+        {children}
+      </div>
+    </>
   );
 }
 
