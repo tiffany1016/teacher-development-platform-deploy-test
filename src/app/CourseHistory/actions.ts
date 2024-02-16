111aa2433dea0409e2bda7ba32cccbf883dc94f7
Shu-Chenliu <susanliu.singing@gmail.com>
2024-02-16 15:09:43 +0800

    fix spacing

--- src/app/CourseHistory/actions.ts
+++ src/app/CourseHistory/actions.ts
@@ -2,20 +2,44 @@ export const getCourse = async(username:string) =>{
   "use server";
   const course= [{
     type:"領導力",
-    topic:"主題一",
+    topic:"主題A1",
     name:"class 1",
   },{
-    type:"領導力",
-    topic:"主題2",
+    type:"教學力",
+    topic:"主題B1",
     name:"class 2",
   },{
     type:"領導力",
-    topic:"主題3",
+    topic:"主題A2",
     name:"class 3",
   },{
     type:"領導力",
-    topic:"主題3",
+    topic:"主題A3",
     name:"class 4",
+  },{
+    type:"領導力",
+    topic:"主題A3",
+    name:"class 5",
+  },{
+    type:"教學力",
+    topic:"主題B2",
+    name:"class 6",
+  },{
+    type:"教學力",
+    topic:"主題B2",
+    name:"class 7",
+  },{
+    type:"CC力",
+    topic:"主題C1",
+    name:"class 8",
+  },{
+    type:"CC力",
+    topic:"主題C1",
+    name:"class 9",
+  },{
+    type:"DD力",
+    topic:"主題D1",
+    name:"class 10",
   }];
   return course;
 }
--- src/app/_components/CourseToggle.tsx
+++ src/app/_components/CourseToggle.tsx
@@ -15,16 +15,16 @@ export default function AccordionDemo({topic,courses,color}:CourseToggleProps) {
   return (
     <Accordion type="single" collapsible className="w-full">
       <AccordionItem value="item-1">
-        <div style={{borderColor:color,borderWidth:4}} className="grid rounded-3xl w-full content-center pr-2">
+        <div style={{borderColor:color,borderWidth:4}} className="grid rounded-3xl w-full pr-2">
           <AccordionTrigger className="">
-            <div className="grid ml-4 mr-2">
-              <p className="text-black text-xl">主題一</p>
+            <div className="grid ml-4 mr-2 content-center">
+              <p className="text-black text-xl">{topic}</p>
             </div>
           </AccordionTrigger>
           <AccordionContent>
-            <div className="ml-4">
+            <div className="ml-4 flex flex-col gap-2">
               {courses.map((course)=>(
-                <Button key={course.name} style={{backgroundColor:color}}>
+                <Button key={course.name} style={{backgroundColor:color}} className="w-2/3">
                   {course.name}
                 </Button>
               ))}
