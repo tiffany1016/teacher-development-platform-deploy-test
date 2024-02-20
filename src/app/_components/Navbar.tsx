9666010de093a9d2dec8cea5ea90ef27a83d7ce5
ining310 <h94usu6@gmail.com>
2024-02-21 03:02:13 +0800

    navbar menu

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -6,12 +6,13 @@ import Image from "next/image"
 import { INDIGO } from "@/lib/constants";
 import LoginDialog from "./LoginDialog";
 import UserAvatar from "./UserAvatar";
+import { NavbarMenu } from "./NavbarMenu";
 
 async function Navbar() {
   const session = await auth();
   const notAuth = (!session || !session?.user?.email);
   return (
-    <nav style={{ backgroundColor: INDIGO }} className="fixed flex justify-between items-center w-full max-w-8xl px-10 py-3 border-b-2 border-gray-100d">
+    <nav style={{ backgroundColor: INDIGO }} className="flex justify-between items-center w-full max-w-8xl px-10 py-3 border-b-2 border-gray-100d">
       <div className="flex gap-3">
         <button className="flex gap-2.5">
           <Link href={`/`} className="flex gap-2">
@@ -30,17 +31,18 @@ async function Navbar() {
       <div className="flex items-center justify-self-end text-white">
         { notAuth ?
           <LoginDialog /> :
-          <div className="flex">
-            <UserAvatar userEmail={session?.user?.email || ""} />
-            <Link href={`/auth/signout`}>
-              <button
-                type={"submit"}
-                className="flex items-center gap-1.5 rounded-sm hover:opacity-80 py-1 px-2 font-semibold"
-              >
-                {session?.user?.username} 登出
-              </button>
-            </Link>
-          </div>
+          <NavbarMenu userEmail={session?.user?.email || ""} username={session?.user?.username || ""} />
+          // <div className="flex">
+          //   <UserAvatar userEmail={session?.user?.email || ""} />
+          //   <Link href={`/auth/signout`}>
+          //     <button
+          //       type={"submit"}
+          //       className="flex items-center gap-1.5 rounded-sm hover:opacity-80 py-1 px-2 font-semibold"
+          //     >
+          //       {session?.user?.username} 登出
+          //     </button>
+          //   </Link>
+          // </div>
         }
       </div>
     </nav>
---
+++ src/app/_components/NavbarMenu.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import UserAvatar from "./UserAvatar"
+import { INDIGO, LIGHT_BLUE } from "@/lib/constants"
+import { Separator } from "@/components/ui/separator"
+
+export function NavbarMenu({userEmail, username}: { userEmail: string , username: string }) {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="bg-transparent hover:bg-transparent active:bg-transparent">
+            <div className="flex">
+              <UserAvatar userEmail={userEmail || ""} />
+              <div
+                className="flex items-center gap-1.5 rounded-sm hover:opacity-80 py-1 px-2 font-semibold"
+              >
+                {username}
+              </div>
+            </div>
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <div className="grid gap-2 p-4 w-32 text-sm" style={{ color: INDIGO, backgroundColor: LIGHT_BLUE }} >
+              <a href="/CourseHistory" className="hover:opacity-70">我的修課紀錄</a>
+              <a href="/MyLectureHistory" className="hover:opacity-70">我的講師紀錄</a>
+              <a href="/" className="hover:opacity-70">我的IDP</a>
+              <Separator className="bg-white opacity-50" />
+              <a href="/" className="hover:opacity-70">關於我</a>
+              <a href="/auth/signout" className="hover:opacity-70">登出</a>
+            </div>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
