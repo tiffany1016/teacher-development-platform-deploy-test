b3451b4d285188218bd8f87822ed9cc40f7d00cc
Shu-Chenliu <susanliu.singing@gmail.com>
2024-02-23 16:45:31 +0800

    p19-20 select

---
+++ src/app/TeacherHistory/components/actions.ts
@@ -0,0 +1,63 @@
+export const getTeachers=()=>{
+  return ["啊一","啊二"];
+}
+export const getTypes=()=>{
+  return ["領導力","教學力","cc力"];
+}
+export const getTopics=()=>{
+  return ["嚴謹教學","個人化學習"];
+}
+export const getSeries=()=>{
+  return ["2023峰會","2024峰會"];
+}
+export const getYears=()=>{
+  return ["112-2","112-1","111-1"];
+}
+export const getCourse=()=>{
+  return ["課程一","課程二","課程三","課程四","課程五","課程六","課程七"];
+}
+export const getData=()=>{
+  return [{
+    type:"領導力",
+    topic:"嚴謹教學",
+    name:"課程一",
+    time:"111-1",
+    teacher:"甲老師",
+    series:"2024峰會",
+  },{
+    type:"領導力",
+    topic:"嚴謹教學",
+    name:"課程二",
+    time:"111-2",
+    teacher:"乙老師",
+    series:"2024峰會",
+  },{
+    type:"領導力",
+    topic:"個人化學習",
+    name:"課程三",
+    time:"111-1",
+    teacher:"丙老師",
+    series:"2024峰會",
+  },{
+    type:"領導力",
+    topic:"個人化學習",
+    name:"課程四",
+    time:"111-1",
+    teacher:"丁老師",
+    series:"2024峰會",
+  },{
+    type:"領導力",
+    topic:"個人化學習",
+    name:"課程五",
+    time:"111-1",
+    teacher:"戊老師",
+    series:"2024峰會",
+  },{
+    type:"領導力",
+    topic:"個人化學習",
+    name:"課程六",
+    time:"111-1",
+    teacher:"己老師",
+    series:"2024峰會",
+  }];
+}
