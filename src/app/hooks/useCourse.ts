c2941858128b4b0f4807b08a5a37cdb85ae718d6
susan <h811124@nehs.hc.edu.tw>
2024-03-13 10:08:07 +0800

    user course record hooks

---
+++ src/app/hooks/useCourse.ts
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function useCourse() {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const postCourse = async ({
+    year,
+    series,
+    courseId,
+    name,
+    teacherId,
+    typeId,
+  }: {
+    year: number,
+    series:string,
+    courseId:string,
+    name:string,
+    teacherId:string,
+    typeId:number,
+  }) => {
+    setLoading(true);
+
+    const res = await fetch("/api/course", {
+      method: "POST",
+      body: JSON.stringify({
+        year,
+        series,
+        courseId,
+        name,
+        teacherId,
+        typeId,
+      }),
+    });
+
+    if (!res.ok) {
+      const body = await res.json();
+      throw new Error(body.error);
+    }
+
+    // router.refresh() is a Next.js function that refreshes the page without
+    // reloading the page. This is useful for when we want to update the UI
+    // from server components.
+    router.refresh();
+    setLoading(false);
+  };
+  const updateCourse = async ({
+    courseId,
+    name,
+    typeId,
+  }: {
+    courseId: string,
+    name: string,
+    typeId: number,
+  }) => {
+    setLoading(true);
+
+    const res = await fetch("/api/course", {
+      method: "PUT",
+      body: JSON.stringify({
+        courseId,
+        name,
+        typeId,
+      }),
+    });
+
+    if (!res.ok) {
+      const body = await res.json();
+      throw new Error(body.error);
+    }
+
+    // router.refresh() is a Next.js function that refreshes the page without
+    // reloading the page. This is useful for when we want to update the UI
+    // from server components.
+    router.refresh();
+    setLoading(false);
+  };
+  const deleteCourse = async ({
+    courseId,
+  }: {
+    courseId: string,
+  }) => {
+    setLoading(true);
+
+    const res = await fetch("/api/course", {
+      method: "DELETE",
+      body: JSON.stringify({
+        courseId,
+      }),
+    });
+
+    if (!res.ok) {
+      const body = await res.json();
+      throw new Error(body.error);
+    }
+
+    // router.refresh() is a Next.js function that refreshes the page without
+    // reloading the page. This is useful for when we want to update the UI
+    // from server components.
+    router.refresh();
+    setLoading(false);
+  };
+
+
+  return {
+    postCourse,
+    updateCourse,
+    deleteCourse,
+    loading,
+  };
+}
---
+++ src/app/hooks/useRecord.ts
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function useRecord() {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const postRecord = async ({
+    studentId,
+    courseId,
+    title,
+    discription,
+    link,
+    publicToEveryone,
+  }: {
+    studentId: string,
+    courseId: string,
+    title: string,
+    discription?: string,
+    link?: string,
+    publicToEveryone?: boolean,
+  }) => {
+    setLoading(true);
+
+    const res = await fetch("/api/courseRecord", {
+      method: "POST",
+      body: JSON.stringify({
+        studentId,
+        courseId,
+        title,
+        discription,
+        link,
+        publicToEveryone,
+      }),
+    });
+
+    if (!res.ok) {
+      const body = await res.json();
+      throw new Error(body.error);
+    }
+
+    // router.refresh() is a Next.js function that refreshes the page without
+    // reloading the page. This is useful for when we want to update the UI
+    // from server components.
+    router.refresh();
+    setLoading(false);
+  };
+  const updateCourse = async ({
+    studentId,
+    courseId,
+    title,
+    discription,
+    link,
+    publicToEveryone,
+  }: {
+    studentId: string,
+    courseId: string,
+    title?: string,
+    discription?: string,
+    link?: string,
+    publicToEveryone?: boolean,
+  }) => {
+    setLoading(true);
+
+    const res = await fetch("/api/courseRecord", {
+      method: "PUT",
+      body: JSON.stringify({
+        studentId,
+        courseId,
+        title,
+        discription,
+        link,
+        publicToEveryone,
+      }),
+    });
+
+    if (!res.ok) {
+      const body = await res.json();
+      throw new Error(body.error);
+    }
+
+    // router.refresh() is a Next.js function that refreshes the page without
+    // reloading the page. This is useful for when we want to update the UI
+    // from server components.
+    router.refresh();
+    setLoading(false);
+  };
+  const deleteCourse = async ({
+    studentId,
+    courseId,
+  }: {
+    studentId: string,
+    courseId: string,
+  }) => {
+    setLoading(true);
+
+    const res = await fetch("/api/courseRecord", {
+      method: "DELETE",
+      body: JSON.stringify({
+        studentId,
+        courseId,
+      }),
+    });
+
+    if (!res.ok) {
+      const body = await res.json();
+      throw new Error(body.error);
+    }
+
+    // router.refresh() is a Next.js function that refreshes the page without
+    // reloading the page. This is useful for when we want to update the UI
+    // from server components.
+    router.refresh();
+    setLoading(false);
+  };
+
+
+  return {
+    postRecord,
+    updateCourse,
+    deleteCourse,
+    loading,
+  };
+}
